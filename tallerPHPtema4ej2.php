99edf87e772afe3ecf3e0bc32e358270c50ced34
MartaPoo <168516237+MartaPoo@users.noreply.github.com>
2024-10-11 03:13:07 -0700

    Ejercicios

---
+++ tallerPHPtema4ej2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Tema 4 - ejercicio 2</h1>
+        <p>Ejercicio con Operadores de Comparación y Lógicos</p>
+
+        <?php 
+            $a = 1;
+            $b = 2;
+            $c = 3;
+
+            if($a>$b && $c<$a){
+                echo "ambas condiciones son verdaderas";
+            } else {
+                echo "alguna de las condiciones es falsa";
+            }
+
+            
+        ?>
+           
+    </body>
+</html>
